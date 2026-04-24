babfe5dc1d7729152e13e6c8f2d9a9d452f5d315
AmgothNithin <91463991+AmgothNithin@users.noreply.github.com>
2026-04-24 23:36:56 +0530

    Update OrderServiceApplicationTests.java

--- order-service/src/test/java/com/nithin/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/nithin/orderservice/OrderServiceApplicationTests.java
@@ -1,15 +1,10 @@
-// package com.nithin.orderservice;
+package com.nithin.orderservice;
 
 import org.junit.jupiter.api.Disabled;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Disabled
 class OrderServiceApplicationTests {
-
-	// @Test
-	// void contextLoads() {
-	// }
-
 }
+
