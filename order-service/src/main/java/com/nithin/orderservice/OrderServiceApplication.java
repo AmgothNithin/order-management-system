8c622848339d009702bd0d29eeea160146434cd2
AmgothNithin <91463991+AmgothNithin@users.noreply.github.com>
2026-04-25 00:05:44 +0530

    Update OrderServiceApplication.java

--- order-service/src/main/java/com/nithin/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/nithin/orderservice/OrderServiceApplication.java
@@ -1,15 +1,17 @@
 
+
+
 package com.nithin.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
-@SpringBootApplication
-@EnableAutoConfiguration(exclude = {
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication(
+    excludeName = {
+        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration",
+        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+    }
+)
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
